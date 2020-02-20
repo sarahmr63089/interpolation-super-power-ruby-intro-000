14ee7816541282631cd20c8d767b6760ce6a6b9c
Sarah Rose <sarahrose63089@gmail.com>
2020-02-20 02:18:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 
 rainbow = [color = red, color = orange, color = yellow, color = green, color = blue, color = indigo, color = violet]
 
-def display_rainbow([color = red, color = orange, color = yellow, color = green, color = blue, color = indigo, color = violet])
+def display_rainbow(rainbow)
     puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 end
